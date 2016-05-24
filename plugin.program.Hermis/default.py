326beb255fe5da8e3e1a45e240409865e386d86c
Twilight0 <twilight@freemail.gr>
2016-05-24 19:01:16 +0300

    Fix

--- plugin.program.Hermis/default.py
+++ plugin.program.Hermis/default.py
@@ -11,6 +11,7 @@ import ntpath
 
 
 USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+base='https://github.com/Twilight0/repo.twilight0/'
 ADDON=xbmcaddon.Addon(id='plugin.program.Hermis')
 dialog = xbmcgui.Dialog()    
 VERSION = "1.0.3"
